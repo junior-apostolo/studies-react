5ba312a1829af030bd398f0bee84cfa4c1109222
junior-apostolo <juniorapostolo2010@gmail.com>
2024-07-01 23:41:11 -0300

    feat: iniciando react hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+// import { Component } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [reverse, setReverse] = useState(false);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+        <h1>contador: {count}</h1>
+
+        <button type="button" onClick={handleClick}>
+          Reverse
+        </button>
       </header>
     </div>
   );
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       reverse: true,
+//     };
+//   }
+
+//   render() {
+//     const { reverse } = this.state;
+//     const reverseClass = reverse ? 'reverse' : '';
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+//           <button type='button' onClick={this.setState}>reverse</button>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
