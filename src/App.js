1d5a58ef43c9ae2e3dff5041882d9d81fbd43006
junior-apostolo <juniorapostolo2010@gmail.com>
2024-07-08 01:15:58 -0300

    feat: learning about useCallback()

--- src/App.js
+++ src/App.js
@@ -1,56 +1,36 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useCallback } from 'react';
+import P from 'prop-types';
 // import { Component } from 'react';
 
+const Button = ({ incrementButton }) => {
+  return <button onClick={() => incrementButton(10)}>+</button>;
+};
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
+
+// O componente Pai sempre sera renderizado quando o estado mudar
 function App() {
   const [count, setCount] = useState(0);
 
-  // Executa somente quando a dependencia mudar
-  useEffect(() => {
-    console.log('componentDidUpdate: somente quando a dependencia mudar');
-  }, [count]);
+  // useCallback() => utilizado para memoizar(cachear) uma função entre novas renderizações
+  // que é passada para um componente filho.
 
-  // Executa apenas uma vez
-  // useEffect(() => {
-  //   console.log('componentDidUpdate: Executa apenas uma vez');
-  // }, []);
-
-  // Executa sempre que o componente App atualiza
-  // useEffect(() => {
-  //   console.log('componentDidUpdate: sempre que o componente App atualiza');
-  // });
+  // Para que essa função não atualize é utilizado uma função de callback para o estado
+  // assim temos acesso ao valor anterior e atual do estado. Consequentemente fazendo com que
+  // a função não dependa de count, e assim impedindo que seja atualizada sempre que o estado for alterado.
+  const incrementCounter = useCallback((num) => {
+    setCount((c) => c + num);
+  }, []);
 
   return (
     <div className="App">
       <h1>Contador: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <Button incrementButton={incrementCounter}>+</Button>
     </div>
   );
 }
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       reverse: true,
-//     };
-//   }
-
-//   render() {
-//     const { reverse } = this.state;
-//     const reverseClass = reverse ? 'reverse' : '';
-
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-//           <button type='button' onClick={this.setState}>reverse</button>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
