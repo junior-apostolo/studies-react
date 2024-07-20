ca1f31b4ffd39b3236c31bfd339f6158db6f504a
junior-apostolo <juniorapostolo2010@gmail.com>
2024-07-20 12:40:28 -0300

    feat: learning about useMemo()

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
 import './App.css';
-import { useState, useCallback } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import P from 'prop-types';
 // import { Component } from 'react';
 
-const Button = ({ incrementButton }) => {
-  return <button onClick={() => incrementButton(10)}>+</button>;
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 // O componente Pai sempre sera renderizado quando o estado mudar
 function App() {
-  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  // useCallback() => utilizado para memoizar(cachear) uma função entre novas renderizações
-  // que é passada para um componente filho.
-
-  // Para que essa função não atualize é utilizado uma função de callback para o estado
-  // assim temos acesso ao valor anterior e atual do estado. Consequentemente fazendo com que
-  // a função não dependa de count, e assim impedindo que seja atualizada sempre que o estado for alterado.
-  const incrementCounter = useCallback((num) => {
-    setCount((c) => c + num);
+  // Component did mount
+  useEffect(() => {
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
+
+  console.log('Pai, renderizou');
 
   return (
     <div className="App">
-      <h1>Contador: {count}</h1>
-      <Button incrementButton={incrementCounter}>+</Button>
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
